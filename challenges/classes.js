692c5a85ab25b18ba47815e2b5f70846dcffd398
CJ Lucido <c.jose.lucido@gmail.com>
2019-09-20 09:41:40 -0700

    Completed final stretch goal, may go back and refactor my dinosaurs to be constructor functions

--- challenges/classes.js
+++ challenges/classes.js
@@ -42,3 +42,25 @@ console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+class CubeMaker extends CuboidMaker{
+    constructor(attr){
+        super(attr);
+    }
+
+    volume(){
+        return this.length * this.height * this.width;
+    }
+    surfaceArea(){
+        return 6 * (this.length * this.length);
+    }
+}
+
+const cube = new CubeMaker({
+    length: 5,
+    width: 5,
+    height: 5
+  })
+
+console.log(cube.volume()); // 75
+console.log(cube.surfaceArea()); // 150
